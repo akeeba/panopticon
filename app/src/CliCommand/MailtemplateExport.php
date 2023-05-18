b786c7c76741fc6f3f32eeae4c1723bfb83ea477
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-05-18 19:45:33 +0300

    Install mail templates

---
+++ app/src/CliCommand/MailtemplateExport.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * @package   panopticon
+ * @copyright Copyright (c)2023-2023 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\Panopticon\CliCommand;
+
+defined('AKEEBA') || die;
+
+use Akeeba\Panopticon\Factory;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+	name: "mailtemplate:export",
+	description: "Exports the mail templates as a SQL file"
+)]
+class MailtemplateExport extends AbstractCommand
+{
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$generic = $input->getOption('generic');
+		$db = Factory::getContainer()->db;
+		$query = $db->getQuery(true)
+			->select('*')
+			->from($db->quoteName('#__mailtemplates'));
+
+		foreach ($db->setQuery($query)->loadObjectList() as $item)
+		{
+			$sql = $db->getQuery(true)
+				->insert($db->quoteName('#__mailtemplates'))
+				->columns([
+					$db->quoteName('type'),
+					$db->quoteName('language'),
+					$db->quoteName('subject'),
+					$db->quoteName('html'),
+					$db->quoteName('plaintext'),
+				])->values(
+					$db->quote($item->type) . ',' .
+					$db->quote($item->language) . ',' .
+					$db->quote($item->subject) . ',' .
+					$db->quote($item->html) . ',' .
+					$db->quote($item->plaintext)
+				);
+
+			$sql = (string) $sql;
+
+			if (!$generic)
+			{
+				$sql = $db->replacePrefix($sql);
+			}
+
+			echo $sql . PHP_EOL;
+		}
+
+		$query = $db->getQuery(true)
+			->select('*')
+			->from($db->quoteName('#__akeeba_common'))
+			->where($db->quoteName('key') . ' = ' . $db->quote('mail.common_css'));
+		$cssObject = $db->setQuery($query)->loadObject();
+
+		if ($cssObject)
+		{
+			$sql = $db->getQuery(true)
+				->insert($db->quoteName('#__akeeba_common'))
+				->columns([
+					$db->quoteName('key'),
+					$db->quoteName('value'),
+				])
+				->values(
+					$db->quote($cssObject->key) . ',' .
+					$db->quote($cssObject->value)
+				);
+
+			$sql = (string) $sql;
+
+			if (!$generic)
+			{
+				$sql = $db->replacePrefix($sql);
+			}
+
+			echo $sql . PHP_EOL;
+		}
+
+		return Command::SUCCESS;
+	}
+
+	protected function configure(): void
+	{
+		$this
+			->addOption('generic', 'g', InputOption::VALUE_NEGATABLE, 'Use a generic database table prefix (#__)', false);
+	}
+}
