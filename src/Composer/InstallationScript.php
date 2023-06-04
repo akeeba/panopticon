ae567277b5b417e20acafe7c4b7b943555e5c55d
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-06-04 12:01:40 +0300

    Use Composer hooks (#35)
    * Composer scripts
    
    Signed-off-by: Nicholas K. Dionysopoulos <nicholas@akeeba.com>
    
    * Remove 3PD files from repo
    
    Signed-off-by: Nicholas K. Dionysopoulos <nicholas@akeeba.com>
    
    * Fix recursive file copy
    
    Signed-off-by: Nicholas K. Dionysopoulos <nicholas@akeeba.com>
    
    * Update composer.json
    
    Signed-off-by: Nicholas K. Dionysopoulos <nicholas@akeeba.com>
    
    * Use Composer extra for file copy
    
    Signed-off-by: Nicholas K. Dionysopoulos <nicholas@akeeba.com>
    
    * Use Composer extra for Babel and SCSS compilation
    
    Signed-off-by: Nicholas K. Dionysopoulos <nicholas@akeeba.com>
    
    * Simplify the build script
    
    Now that `composer install` does most of the work we do not need to do
    that in the Phing build script.
    
    Signed-off-by: Nicholas K. Dionysopoulos <nicholas@akeeba.com>
    
    ---------
    
    Signed-off-by: Nicholas K. Dionysopoulos <nicholas@akeeba.com>

---
+++ src/Composer/InstallationScript.php
@@ -0,0 +1,332 @@
+<?php
+/**
+ * @package   panopticon
+ * @copyright Copyright (c)2023-2023 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   https://www.gnu.org/licenses/agpl-3.0.txt GNU Affero General Public License, version 3 or later
+ */
+
+namespace Akeeba\Panopticon\Composer;
+
+use Akeeba\Panopticon\Container;
+use Akeeba\Panopticon\Factory;
+use Composer\Script\Event;
+use Symfony\Component\Finder\Finder;
+use function Symfony\Component\VarDumper\Dumper\esc;
+
+abstract class InstallationScript
+{
+	/**
+	 * Run actions after Composer Update
+	 *
+	 * @param   Event  $event
+	 *
+	 * @return  void
+	 * @noinspection PhpUnused
+	 */
+	public static function postComposerUpdate(Event $event): void
+	{
+		self::emptyReleaseDirectory($event);
+		self::copyHtaccessIntoVendor($event);
+		self::removeDSStore($event);
+	}
+
+	/**
+	 * Copies static files pulled in via NPM to their respective locations
+	 *
+	 * @param   Event  $event
+	 *
+	 * @return  void
+	 * @noinspection PhpUnused
+	 */
+	public static function copyNodeDependencies(Event $event): void
+	{
+		$io = $event->getIO();
+		$io->debug('Copying static files');
+
+		$container = self::getAWFContainer();
+
+		$extras = $event->getComposer()->getPackage()->getExtra();
+
+		foreach ($extras['copy-static'] ?? [] as $copyDef)
+		{
+			$type  = $copyDef['type'] ?? 'file';
+			$from  = $copyDef['from'] ?? '';
+			$to    = $copyDef['to'] ?? '';
+			$names = $copyDef['names'] ?? null;
+
+			$from = $container->basePath . '/' . trim($from, '/');
+			$to   = $container->basePath . '/' . trim($to, '/');
+
+			if (empty($from) || empty($to))
+			{
+				continue;
+			}
+
+			if ($type === 'file')
+			{
+				$container->fileSystem->copy($from, $to);
+
+				continue;
+			}
+
+			self::copyFiles($from, $to, $names);
+		}
+	}
+
+	public static function babel(Event $event): void
+	{
+		$io = $event->getIO();
+		$io->debug('Compiling JavaScript files');
+
+		$container = self::getAWFContainer();
+		$extras    = $event->getComposer()->getPackage()->getExtra();
+
+		foreach ($extras['babel'] ?? [] as $definition)
+		{
+			$folder  = trim($definition['folder'], '/');
+			$outdir  = trim($definition['outdir'], '/');
+			$names   = $definition['names'] ?? ['*.js'];
+			$exclude = $definition['exclude'] ?? null;
+
+			if (empty($folder) || empty($outdir))
+			{
+				continue;
+			}
+
+			$folder = $container->basePath . '/' . $folder;
+			$outdir = $container->basePath . '/' . $outdir;
+
+			$finder = new Finder();
+			$finder->ignoreDotFiles(true)
+				->ignoreVCS(true)
+				->ignoreVCSIgnored(true)
+				->in($folder)
+				->name($names)
+				->files();
+
+			if ($exclude)
+			{
+				$finder->notName($exclude);
+			}
+
+			if (!$finder->hasResults())
+			{
+				continue;
+			}
+
+			foreach ($finder as $file)
+			{
+				$inFile  = $file->getPathname();
+				$outFile = $file->getPath() . '/' . $file->getBasename('.js') . '.min.js';
+
+				if (file_exists($outFile) && filemtime($outFile) >= filemtime($inFile))
+				{
+					continue;
+				}
+
+				$cwd = getcwd();
+				chdir($container->basePath);
+
+				$command = 'npx babel ' . escapeshellarg($inFile) . ' --out-dir ' . escapeshellarg($outdir)
+					. ' --out-file-extension ' . escapeshellarg('.min.js') . ' --source-maps';
+
+				passthru($command);
+
+				chdir($cwd);
+			}
+		}
+	}
+
+	public static function sass(Event $event): void
+	{
+		$io = $event->getIO();
+		$io->debug('Compiling SCSS files');
+
+		$container = self::getAWFContainer();
+		$extras    = $event->getComposer()->getPackage()->getExtra();
+
+		foreach ($extras['scss'] ?? [] as $definition)
+		{
+			$folder  = trim($definition['folder'], '/');
+			$outdir  = trim($definition['outdir'], '/');
+			$names   = $definition['names'] ?? ['*.js'];
+			$exclude = $definition['exclude'] ?? null;
+
+			if (empty($folder) || empty($outdir))
+			{
+				continue;
+			}
+
+			$folder = $container->basePath . '/' . $folder;
+			$outdir = $container->basePath . '/' . $outdir;
+
+			$finder = new Finder();
+			$finder->ignoreDotFiles(true)
+				->ignoreVCS(true)
+				->ignoreVCSIgnored(true)
+				->in($folder)
+				->name($names)
+				->files();
+
+			if ($exclude)
+			{
+				$finder->notName($exclude);
+			}
+
+			if (!$finder->hasResults())
+			{
+				continue;
+			}
+
+			foreach ($finder as $file)
+			{
+				$inFile  = $file->getPathname();
+				$outFile = $file->getPath() . '/' . $file->getBasename('.js') . '.min.js';
+
+				if (file_exists($outFile) && filemtime($outFile) >= filemtime($inFile))
+				{
+					continue;
+				}
+
+				$cwd = getcwd();
+				chdir($container->basePath);
+
+				$command = 'sass ' . escapeshellarg($inFile . ':' . $outFile) .
+					' -s compressed --update';
+
+				passthru($command);
+
+				chdir($cwd);
+			}
+		}
+	}
+
+
+	/**
+	 * Get the Container object of the application
+	 *
+	 * @return  Container
+	 */
+	private static function getAWFContainer(): Container
+	{
+		defined('AKEEBA') || define('AKEEBA', 1);
+
+		require_once __DIR__ . '/../../defines.php';
+		@include_once __DIR__ . '/../../version.php';
+
+		defined('AKEEBA_PANOPTICON_VERSION') || define('AKEEBA_PANOPTICON_VERSION', '0.0.0-dev-installing');
+		defined('AKEEBA_PANOPTICON_DATE') || define('AKEEBA_PANOPTICON_DATE', gmdate('Y-m-d'));
+		defined('AKEEBA_PANOPTICON_CODENAME') || define('AKEEBA_PANOPTICON_CODENAME', 'Aphelion');
+		defined('AKEEBA_PANOPTICON_MINPHP') || define('AKEEBA_PANOPTICON_MINPHP', PHP_VERSION);
+
+		return Factory::getContainer();
+	}
+
+	private static function emptyReleaseDirectory(Event $event): void
+	{
+		$io = $event->getIO();
+		$io->debug('Emptying the release directory');
+
+		$container = self::getAWFContainer();
+
+		$container->fileSystem->rmdir($container->basePath . '/release');
+		$container->fileSystem->mkdir($container->basePath . '/release');
+	}
+
+	private static function copyHtaccessIntoVendor(Event $event)
+	{
+		$io = $event->getIO();
+		$io->debug('Copying .htaccess and web.config into the vendor folder');
+
+		$container = self::getAWFContainer();
+
+		$container->fileSystem->copy(
+			$container->basePath . '/src/.htaccess',
+			$container->basePath . '/vendor/.htaccess'
+		);
+		$container->fileSystem->copy(
+			$container->basePath . '/src/web.config',
+			$container->basePath . '/vendor/web.config'
+		);
+	}
+
+	private static function removeDSStore(Event $event)
+	{
+		$io = $event->getIO();
+		$io->debug('Removing .DS_Store files');
+
+		$container = self::getAWFContainer();
+
+		$finder = new Finder();
+		$finder->ignoreDotFiles(false)
+			->ignoreVCS(true)
+			->ignoreVCSIgnored(true)
+			->in([
+				$container->basePath . '/assets',
+				$container->basePath . '/build',
+				$container->basePath . '/cache',
+				$container->basePath . '/cli',
+				$container->basePath . '/includes',
+				$container->basePath . '/languages',
+				$container->basePath . '/log',
+				$container->basePath . '/media',
+				$container->basePath . '/src',
+				$container->basePath . '/templates',
+				$container->basePath . '/user_code',
+				$container->basePath . '/ViewTemplates',
+			])
+			->name('.DS_Store')
+			->files();
+
+		if (!$finder->hasResults())
+		{
+			return;
+		}
+
+		foreach ($finder as $item)
+		{
+			$container->fileSystem->delete($item->getPathname());
+		}
+	}
+
+	private static function copyFiles(string $from, string $to, ?array $names = null, ?callable $customiser = null)
+	{
+		$container = self::getAWFContainer();
+
+		$finder = new Finder();
+		$finder->ignoreDotFiles(true)
+			->ignoreVCS(true)
+			->ignoreVCSIgnored(true)
+			->in($from)
+			->files();
+
+		if (!empty($names))
+		{
+			$finder->name($names);
+		}
+
+		if (is_callable($customiser))
+		{
+			call_user_func($customiser, $finder);
+		}
+
+		if (!$finder->hasResults())
+		{
+			return;
+		}
+
+		foreach ($finder as $file)
+		{
+			$target = rtrim($to, '/') . '/' . $file->getRelativePathname();
+
+			$targetPath = dirname($target);
+
+			if (!file_exists($targetPath))
+			{
+				$container->fileSystem->mkdir($targetPath);
+			}
+
+			$container->fileSystem->copy($file->getPathname(), $target);
+		}
+	}
+}
