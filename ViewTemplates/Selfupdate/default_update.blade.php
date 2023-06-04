198739b46d2ef5ec87d6e16b4062c1f7b08c47c1
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-06-04 13:00:40 +0300

    Improve the update page

--- ViewTemplates/Selfupdate/default_update.blade.php
+++ ViewTemplates/Selfupdate/default_update.blade.php
@@ -45,3 +45,32 @@ defined('AKEEBA') || die;
         </a>
     </div>
 </div>
+
+<div class="card card-body border-info bg-body-tertiary mb-5">
+    <h4 class="card-title">
+        <span class="fa fa-info-circle pe-1 text-info"></span>
+        @lang('PANOPTICON_APP_TITLE_SHORT') {{ $this->latestversion->version }}
+    </h4>
+    <p>
+        @sprintf('PANOPTICON_SELFUPDATE_LBL_UPTODATE_RELEASED', Html::date($this->latestversion->releaseDate, \Awf\Text\Text::_('DATE_FORMAT_LC1')))
+        <br />
+        @lang('PANOPTICON_SELFUPDATE_LBL_WILL_BE_DOWNLOADED_FROM')
+        <a href="{{ $this->latestversion->downloadUrl }}" target="_blank" class="font-monospace small text-decoration-none text-secondary">
+            {{ $this->latestversion->downloadUrl }}
+        </a>
+    </p>
+    <p>
+        <a href="{{ $this->latestversion->infoUrl }}" target="_blank">@lang('PANOPTICON_SELFUPDATE_LBL_INFORMATION')</a>
+        <span class="fa fa-external-link text-muted small" aria-hidden="true"></span>
+        &bullet;
+        <a href="#releaseNotes"
+           data-bs-toggle="collapse" data-bs-target="#releaseNotes" aria-expanded="false"
+           aria-controls="releaseNotes"
+        >
+            @lang('PANOPTICON_SELFUPDATE_LBL_RELEASE_NOTES')
+        </a>
+    </p>
+    <div id="releaseNotes" class="m-2 px-3 py-2 bg-body border border-info rounded-3 collapse">
+        {{ (new \League\CommonMark\GithubFlavoredMarkdownConverter([]))->convert($this->latestversion->releaseNotes) }}
+    </div>
+</div>
