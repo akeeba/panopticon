32e1cb136dfee825e655bc109696f415d2e1e0ed
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-06-09 09:35:35 +0300

    Add hints for iconography in Tasks page

--- ViewTemplates/Tasks/default.blade.php
+++ ViewTemplates/Tasks/default.blade.php
@@ -224,13 +224,21 @@ $i     = 1;
                 <td>
                     @if ($status->value !== Status::WILL_RESUME->value)
                         <div class="fw-semibold">
-                            <span class="fa fa-clock" aria-hidden="true"></span>
+                            <span class="fa fa-clock" aria-hidden="true"
+                                  data-bs-toggle="tooltip" data-bs-placement="bottom"
+                                  data-bs-title="@lang('PANOPTICON_TASKS_LBL_LAST_RUN')"
+                            ></span>
+                            <span class="visually-hidden">@lang('PANOPTICON_TASKS_LBL_LAST_RUN')</span>
                             {{ $task->last_execution ? HtmlHelper::date($task->last_execution, Text::_('DATE_FORMAT_LC6') . ' T') : '&mdash;' }}
                         </div>
                     @endif
                     @if ($task->enabled)
                         <div class="text-info mt-1">
-                            <span class="fa fa-clock-rotate-left" aria-hidden="true"></span>
+                            <span class="fa fa-clock-rotate-left" aria-hidden="true"
+                                  data-bs-toggle="tooltip" data-bs-placement="bottom"
+                                  data-bs-title="@lang('PANOPTICON_TASKS_LBL_NEXT_RUN')"
+                            ></span>
+                            <span class="visually-hidden">@lang('PANOPTICON_TASKS_LBL_NEXT_RUN')</span>
                             {{ $task->next_execution ? HtmlHelper::date($task->next_execution, Text::_('DATE_FORMAT_LC6') . ' T') : '&mdash;' }}
                         </div>
                     @endif
