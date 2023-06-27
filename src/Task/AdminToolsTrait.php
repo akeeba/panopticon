0d59b730ea73e327757d728cd646ceee1f281a18
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-06-27 18:38:00 +0300

    Joomla 3: determine whether Admin Tools exists

--- src/Task/AdminToolsTrait.php
+++ src/Task/AdminToolsTrait.php
@@ -7,15 +7,30 @@
 
 namespace Akeeba\Panopticon\Task;
 
+defined('AKEEBA') || die;
 
 use Akeeba\Panopticon\Model\Site;
-
-defined('AKEEBA') || die;
 
 trait AdminToolsTrait
 {
 	protected function hasAdminTools(Site $site, bool $onlyProfessional = true)
 	{
+		// Joomla 3 doesn't support Download Keys, so we test the package name instead.
+		if (version_compare($site->getConfig()->get('core.current.version', '4.0.0'), '3.99999.99999', 'le'))
+		{
+			return array_reduce(
+				(array) $site->getConfig()->get('extensions.list'),
+				fn(bool $carry, object $item) => $carry ||
+					(
+						$item->type === 'package'
+						&& $item->element === 'pkg_admintools'
+						&& (!$onlyProfessional || str_contains(strtolower($item->description), 'admin tools professional'))
+					),
+				false
+			);
+		}
+
+		// Joomla 4 and later, we just check if the package supports download keys.
 		return array_reduce(
 			(array) $site->getConfig()->get('extensions.list'),
 			fn(bool $carry, object $item) => $carry ||
