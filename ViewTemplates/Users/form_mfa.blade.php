537ec88db1530ee07498f679ad95539f5a360d15
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-08-04 20:38:38 +0300

    MFA: PHP warning when no created or last used date is recorded

--- ViewTemplates/Users/form_mfa.blade.php
+++ ViewTemplates/Users/form_mfa.blade.php
@@ -8,13 +8,15 @@
 defined('AKEEBA') || die;
 
 use Akeeba\Panopticon\Factory;
+use Awf\Html\Html;
+use Awf\Text\Text;
 use Awf\Uri\Uri;
 
 /** @var \Akeeba\Panopticon\View\Users\Html $this */
 
 $container = Factory::getContainer();
-$user      = $container->userManager->getUser($this->getModel()->getId());
-$token     = $container->session->getCsrfToken()->getValue();
+$user = $container->userManager->getUser($this->getModel()->getId());
+$token = $container->session->getCsrfToken()->getValue();
 ?>
 
 <div class="card">
@@ -60,130 +62,139 @@ $token     = $container->session->getCsrfToken()->getValue();
         {{-- MFA list --}}
         <div class="my-3 p-2">
             @foreach($this->methods as $methodName => $method)
-            <?php
-                $defaultClass = $this->defaultMethod == $methodName ? 'border-2 border-warning bg-light-subtle' : '';
-                $defaultClass = $methodName === 'backupcodes' ? 'border-danger' : $defaultClass;
-            ?>
-            <div class="border rounded-2 py-2 px-3 {{ $defaultClass }} mb-3">
-                {{-- MFA method header --}}
-                <div class="d-flex flex-row gap-2 align-items-center mb-3">
-                    <img src="{{ Uri::root() . $method['image'] }}" alt="{{{ $method['name'] }}}"
-                         class="img-fluid bg-light p-2 rounded-2" style="min-width: 3em; max-width: 7em">
+                    <?php
+                    $defaultClass = $this->defaultMethod == $methodName ? 'border-2 border-warning bg-light-subtle' : '';
+                    $defaultClass = $methodName === 'backupcodes' ? 'border-danger' : $defaultClass;
+                    ?>
+                <div class="border rounded-2 py-2 px-3 {{ $defaultClass }} mb-3">
+                    {{-- MFA method header --}}
+                    <div class="d-flex flex-row gap-2 align-items-center mb-3">
+                        <img src="{{ Uri::root() . $method['image'] }}" alt="{{{ $method['name'] }}}"
+                             class="img-fluid bg-light p-2 rounded-2" style="min-width: 3em; max-width: 7em">
 
-                    <h4 class="m-0 p-0 flex-grow-1 fs-5">
+                        <h4 class="m-0 p-0 flex-grow-1 fs-5">
                         <span>
                             {{ $method['display'] }}
                         </span>
-                        @if($this->defaultMethod == $methodName)
-                        <span class="fa fa-star text-warning" aria-hidden="true"></span>
-                        @endif
-                    </h4>
+                            @if($this->defaultMethod == $methodName)
+                                <span class="fa fa-star text-warning" aria-hidden="true"></span>
+                            @endif
+                        </h4>
 
-                    <span class="fa fa-info-circle" aria-hidden="true"
-                          data-bs-toggle="tooltip" data-bs-placement="left"
-                          data-bs-title="<?= $this->escape($method['shortinfo']) ?>"></span>
-                </div>
+                        <span class="fa fa-info-circle" aria-hidden="true"
+                              data-bs-toggle="tooltip" data-bs-placement="left"
+                              data-bs-title="<?= $this->escape($method['shortinfo']) ?>"></span>
+                    </div>
 
-                {{-- MFA active instances --}}
-                @if (count($method['active']))
-                    <div class="my-2">
-                        @foreach($method['active'] as $record)
-                            <div class="ms-5 d-flex flex-column flex-md-row gap-3 align-items-center">
-                                {{-- Instance header --}}
-                                <div class="flex-grow-1 d-flex flex-column gap-1">
-                                    @if ($methodName == 'backupcodes')
-                                        <div class="p-2 mb-1">
-                                            <span class="fa fa-info-circle" aria-hidden="true"></span>
-                                            @sprintf(
-                                                'PANOPTICON_MFA_LBL_BACKUPCODES_PRINT_PROMPT',
-                                                $container->router->route(sprintf(
-                                                    'index.php?view=mfamethod&task=edit&id=%d&returnurl=%s&user_id=%d',
-                                                    (int) $record->id,
-                                                    base64_encode(Uri::getInstance()->toString()),
-                                                    $user->getId()
-                                                ))
-                                            )
-                                        </div>
-                                    @else
-                                        <div class="loginguard-methods-list-method-record-title-container">
-                                            @if ($record->default)
-                                                <span class="fa fa-star text-body-tertiary" aria-hidden="true"
-                                                      data-bs-toggle="tooltip" data-bs-placement="left"
-                                                      data-bs-title="@lang('PANOPTICON_MFA_LBL_LIST_DEFAULTTAG')"></span>
-                                                <span class="visually-hidden">@lang('PANOPTICON_MFA_LBL_LIST_DEFAULTTAG')</span>
-                                                <span class="fw-semibold">
+                    {{-- MFA active instances --}}
+                    @if (count($method['active']))
+                        <div class="my-2">
+                            @foreach($method['active'] as $record)
+                                <div class="ms-5 d-flex flex-column flex-md-row gap-3 align-items-center">
+                                    {{-- Instance header --}}
+                                    <div class="flex-grow-1 d-flex flex-column gap-1">
+                                        @if ($methodName == 'backupcodes')
+                                            <div class="p-2 mb-1">
+                                                <span class="fa fa-info-circle" aria-hidden="true"></span>
+                                                @sprintf(
+                                                    'PANOPTICON_MFA_LBL_BACKUPCODES_PRINT_PROMPT',
+                                                    $container->router->route(sprintf(
+                                                        'index.php?view=mfamethod&task=edit&id=%d&returnurl=%s&user_id=%d',
+                                                        (int) $record->id,
+                                                        base64_encode(Uri::getInstance()->toString()),
+                                                        $user->getId()
+                                                    ))
+                                                )
+                                            </div>
+                                        @else
+                                            <div class="loginguard-methods-list-method-record-title-container">
+                                                @if ($record->default)
+                                                    <span class="fa fa-star text-body-tertiary" aria-hidden="true"
+                                                          data-bs-toggle="tooltip" data-bs-placement="left"
+                                                          data-bs-title="@lang('PANOPTICON_MFA_LBL_LIST_DEFAULTTAG')"></span>
+                                                    <span class="visually-hidden">@lang('PANOPTICON_MFA_LBL_LIST_DEFAULTTAG')</span>
+                                                    <span class="fw-semibold">
                                                     {{{ $record->title }}}
                                                 </span>
-                                            @else
-                                                {{{ $record->title }}}
-                                            @endif
+                                                @else
+                                                    {{{ $record->title }}}
+                                                @endif
+                                            </div>
+                                        @endif
+
+                                        {{-- Last Used --}}
+                                        <div class="d-flex flex-column flex-md-row align-items-center gap-3 text-muted">
+                                        <span class="flex-grow-1">
+                                            @sprintf(
+                                            'PANOPTICON_MFA_LBL_CREATEDON',
+                                            empty($record->created_on)
+                                                ? '&mdash;'
+                                                : Html::date($record->created_on, Text::_('DATE_FORMAT_LC2'))
+                                            )
+                                        </span>
+                                        <span class="flex-grow-1">
+                                            @sprintf(
+                                                'PANOPTICON_MFA_LBL_LASTUSED',
+                                                empty($record->last_used)
+                                                ? '&mdash;'
+                                                : Html::date($record->last_used, Text::_('DATE_FORMAT_LC2'))
+                                            )
+                                        </span>
                                         </div>
-                                    @endif
-
-                                    {{-- Last Used --}}
-                                    <div class="d-flex flex-column flex-md-row align-items-center gap-3 text-muted">
-                                        <span class="flex-grow-1">
-                                            @sprintf('PANOPTICON_MFA_LBL_CREATEDON', \Awf\Html\Html::date($record->created_on, \Awf\Text\Text::_('DATE_FORMAT_LC2')))
-                                        </span>
-                                        <span class="flex-grow-1">
-                                            @sprintf('PANOPTICON_MFA_LBL_LASTUSED', \Awf\Html\Html::date($record->last_used, \Awf\Text\Text::_('DATE_FORMAT_LC2')))
-                                        </span>
                                     </div>
-                                </div>
 
-                                {{-- Buttons --}}
-                                @if ($methodName != 'backupcodes')
-                                    <div>
-                                        <a href="@route(sprintf(
+                                    {{-- Buttons --}}
+                                    @if ($methodName != 'backupcodes')
+                                        <div>
+                                            <a href="@route(sprintf(
                                         'index.php?view=mfamethod&task=edit&id=%s&returnurl=%s&user_id=%d',
                                         (int) $record->id,
                                         base64_encode(Uri::getInstance()->toString()),
                                         $user->getId()
                                     ))"
-                                           role="button" class="btn btn-outline-primary">
-                                            <span class="fa fa-pencil-alt" aria-hidden="true"></span>
-                                            <span class="visually-hidden">
+                                               role="button" class="btn btn-outline-primary">
+                                                <span class="fa fa-pencil-alt" aria-hidden="true"></span>
+                                                <span class="visually-hidden">
                                                 @lang('PANOPTICON_MFA_LBL_EDIT')
                                             </span>
-                                        </a>
+                                            </a>
 
-                                        @if ($method['canDisable'])
-                                            <a href="@route(sprintf(
+                                            @if ($method['canDisable'])
+                                                <a href="@route(sprintf(
                                         'index.php?view=mfamethod&task=delete&id=%s&returnurl=%s&user_id=%d',
                                         (int) $record->id,
                                         base64_encode(Uri::getInstance()->toString()),
                                         $user->getId()
                                     ))"
-                                               role="button" class="btn btn-outline-danger">
-                                                <span class="fa fa-trash-alt" aria-hidden="true"></span>
-                                                <span class="visually-hidden">
+                                                   role="button" class="btn btn-outline-danger">
+                                                    <span class="fa fa-trash-alt" aria-hidden="true"></span>
+                                                    <span class="visually-hidden">
                                                     @lang('PANOPTICON_MFA_LBL_DELETE')
                                                 </span>
-                                            </a>
-                                        @endif
-                                    </div>
-                                @endif
-                            </div>
-                        @endforeach
-                    </div>
-                @endif
+                                                </a>
+                                            @endif
+                                        </div>
+                                    @endif
+                                </div>
+                            @endforeach
+                        </div>
+                    @endif
 
-                @if (empty($method['active']) || $method['allowMultiple'])
-                    <div class="my-2">
-                        <a href="@route(sprintf(
+                    @if (empty($method['active']) || $method['allowMultiple'])
+                        <div class="my-2">
+                            <a href="@route(sprintf(
                             'index.php?view=mfamethod&task=add&method=%s&returnurl=%s&user_id=%d',
                             urlencode($method['name']),
                             base64_encode(Uri::getInstance()->toString()),
                             $user->getId()
                         ))"
-                           role="button" class="btn btn-primary">
-                            <span class="fa fa-plus-square" aria-hidden="true"></span>
-                            @sprintf('PANOPTICON_MFA_LBL_LIST_ADD_A', $this->escape($method['display']))
-                        </a>
-                    </div>
-                @endif
-            </div>
-
+                               role="button" class="btn btn-primary">
+                                <span class="fa fa-plus-square" aria-hidden="true"></span>
+                                @sprintf('PANOPTICON_MFA_LBL_LIST_ADD_A', $this->escape($method['display']))
+                            </a>
+                        </div>
+                    @endif
+                </div>
 
             @endforeach
         </div>
