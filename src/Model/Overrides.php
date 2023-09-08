b486a2422e9f5a825a49db1be298320c490dbfe4
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-09-08 23:40:51 +0300

    Fix for renamed parameter

--- src/Model/Overrides.php
+++ src/Model/Overrides.php
@@ -44,7 +44,7 @@ class Overrides extends Model
 		$limitStart = $this->getUserStateFromRequest('limitstart', 'limitstart', 0, 'int') ?: 0;
 		$limit      = $this->getUserStateFromRequest('limit', 'limit', 20, 'int') ?: 20;
 
-		return new Pagination($this->count(), $limitStart, $limit, app: $this->getContainer());
+		return new Pagination($this->count(), $limitStart, $limit, 10, $this->getContainer());
 	}
 
 	public function get(): Collection
