fee5a6a01ac7d0b6b3b802f750c0f35d29aa5a43
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2023-11-10 09:25:51 +0200

    Fix language key
    CLoses gh-276
    
    Signed-off-by: Nicholas K. Dionysopoulos <nicholas@akeeba.com>

--- ViewTemplates/Extupdates/default.blade.php
+++ ViewTemplates/Extupdates/default.blade.php
@@ -72,7 +72,7 @@ $willAutoUpdate = function (string $key, ?string $oldVersion, ?string $newVersio
         <div class="d-flex flex-column flex-lg-row justify-content-lg-center gap-2 mt-2">
             <!-- Site -->
             <div>
-                <label class="visually-hidden" for="site_id">@lang('PANOPTICON_EXTUPDATES_LBL_EXT_SITE')</label>
+                <label class="visually-hidden" for="site_id">@lang('PANOPTICON_EXTUPDATES_LBL_SITE')</label>
                 {{  $this->container->html->select->genericList(
                         $mainModel->getSiteNamesForSelect(true, 'PANOPTICON_EXTUPDATES_LBL_SITE_SELECT'),
                         'site_id',
