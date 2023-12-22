ae54de296543b2e91a3448fe4a8972bd63750db6
brian teeman <brian@teeman.net>
2023-12-22 11:24:44 +0000

    Style mime types
    It not necessary to include the mimetypes
    
    Signed-off-by: brian teeman <brian@teeman.net>

--- templates/default/index.php
+++ templates/default/index.php
@@ -58,18 +58,18 @@ $importMap     = TemplateHelper::getImportMapAsJson();
 	<?php endif ?>
 	<?php // Stylesheet files ?>
 	<?php foreach ($this->getStyles() as $url => $params): ?>
-		<link rel="stylesheet" type="<?= $params['mime'] ?>" href="<?= $url ?>"<?= ($params['media']) ? " media=\"{$params['media']}\"" : '' ?><?= ($params['attribs'] ?? null) ? ' ' . \Awf\Utils\ArrayHelper::toString($params['attribs']) : '' ?>>
+		<link rel="stylesheet" href="<?= $url ?>"<?= ($params['media']) ? " media=\"{$params['media']}\"" : '' ?><?= ($params['attribs'] ?? null) ? ' ' . \Awf\Utils\ArrayHelper::toString($params['attribs']) : '' ?>>
 	<?php endforeach ?>
 	<?php // Inline Stylesheets ?>
 	<?php foreach ($this->getStyleDeclarations() as $type => $content): ?>
-		<style type="<?= $type ?>"><?= $content ?></style>
+		<style><?= $content ?></style>
 	<?php endforeach ?>
 	<?php // Script files ?>
 	<?php foreach ($this->getScripts() as $url => $params): ?>
-		<script type="<?= $params['mime'] ?>" src="<?= $url ?>"<?= ($params['defer'] ?? false) ? ' defer="defer"' : '' ?><?= ($params['async'] ?? false) ? ' async="async"' : '' ?>></script>
+		<script src="<?= $url ?>"<?= ($params['defer'] ?? false) ? ' defer="defer"' : '' ?><?= ($params['async'] ?? false) ? ' async="async"' : '' ?>></script>
 	<?php endforeach ?>
 	<?php foreach ($this->getScriptDeclarations() as $type => $content): ?>
-		<script type="<?= $type ?>"><?= $content ?></script>
+		<script><?= $content ?></script>
 	<?php endforeach ?>
 
 	<?php if ($darkModeValue): ?>
