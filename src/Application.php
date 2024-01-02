579ffa382ddf57fb1a0515670ad6df1f6623b5f6
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2024-01-02 11:26:15 +0200

    Fix preloading of font files when debug is disabled
    Close gh-470
    
    Signed-off-by: Nicholas K. Dionysopoulos <nicholas@akeeba.com>

--- src/Application.php
+++ src/Application.php
@@ -803,7 +803,13 @@ class Application extends AWFApplication
 				continue;
 			}
 
-			if (!str_contains($file, '.min.') && !(defined('AKEEBADEBUG') && AKEEBADEBUG))
+			if (
+				!str_contains($file, '.min.') && !(defined('AKEEBADEBUG') && AKEEBADEBUG)
+				&& (
+					str_ends_with($file, '.js') || str_ends_with($file, '.css')
+					|| str_contains($file, '.js?') || str_contains($file, '.css?')
+				)
+			)
 			{
 				$lastDot = strrpos($file, '.');
 				$altFile = substr($file, 0, $lastDot) . '.min' . substr($file, $lastDot);
