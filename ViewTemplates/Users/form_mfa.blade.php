3c0ca0dd1e41a3f8dae8a2372da7d3cc8f8b8027
brian teeman <brian@teeman.net>
2024-01-02 14:51:03 +0000

    missed
    Signed-off-by: brian teeman <brian@teeman.net>

--- ViewTemplates/Users/form_mfa.blade.php
+++ ViewTemplates/Users/form_mfa.blade.php
@@ -68,7 +68,7 @@ $token = $container->session->getCsrfToken()->getValue();
                     {{-- MFA method header --}}
                     <div class="d-flex flex-row gap-2 align-items-center mb-3">
                         <img src="{{ Uri::root() . $method['image'] }}" alt="{{{ $method['name'] }}}"
-                             class="img-fluid bg-light p-2 rounded-2" style="min-width: 3em; max-width: 7em">
+                             class="img-fluid bg-light p-2 rounded-2">
 
                         <h4 class="m-0 p-0 flex-grow-1 fs-5">
                         <span>
