45ddcf5eac919b14684a3701a714f71100e97479
brian teeman <brian@teeman.net>
2024-01-25 08:52:21 +0000

    Overrides
    Signed-off-by: brian teeman <brian@teeman.net>

--- ViewTemplates/Overrides/item.blade.php
+++ ViewTemplates/Overrides/item.blade.php
@@ -10,11 +10,20 @@ defined('AKEEBA') || die;
 /**
  * @var \Akeeba\Panopticon\View\Overrides\Html $this
  */
+
+$favIcon = $this->site->getFavicon(asDataUrl: true, onlyIfCached: true);
+
 ?>
 
-<h3 class="text-body-secondary border-bottom border-2 border-info-subtle">
-    <span class="text-body-tertiary me-2">#{{ (int) $this->site->id }}</span>
-    {{ $this->site->name }}
+<h3 class="mt-2 pb-1 border-bottom border-3 border-primary-subtle d-flex flex-row align-items-center gap-2">
+    <span class="text-muted fw-light fs-4">#{{ (int) $this->site->id }}</span>
+    @if($favIcon)
+        <img src="{{{ $favIcon }}}"
+             style="max-width: 1em; max-height: 1em; aspect-ratio: 1.0"
+             class="mx-1 p-1 border rounded"
+             alt="">
+    @endif
+    <span class="flex-grow-1">{{{ $this->site->name }}}</span>
 </h3>
 
 @if($this->item instanceof Throwable || empty($this->item))
