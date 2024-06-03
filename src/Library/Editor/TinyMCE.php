4836ac27df4217380180f896b2b4fb0a5597e69f
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2024-06-03 14:24:53 +0300

    + Load TinyMCE translations
    Signed-off-by: Nicholas K. Dionysopoulos <nicholas@akeeba.com>

--- src/Library/Editor/TinyMCE.php
+++ src/Library/Editor/TinyMCE.php
@@ -11,6 +11,7 @@ defined('AKEEBA') || die;
 
 use Akeeba\Panopticon\Factory;
 use Awf\Container\Container;
+use Awf\Filesystem\File;
 use Awf\Uri\Uri;
 use Awf\Utils\Template;
 use Delight\Random\Random;
@@ -23,6 +24,79 @@ abstract class TinyMCE
 	private static bool $hasInitialised = false;
 
 	private static Container $container;
+
+	/**
+	 * TinyMCE language map
+	 * @var string[]
+	 * @see https://www.tiny.cloud/get-tiny/language-packages/
+	 */
+	private const TINYMCE_LANG_MAP = [
+		'ar'    => 'PANOPTICON_TINYMCE_LANG_AR',
+		'ar_SA' => 'PANOPTICON_TINYMCE_LANG_AR_SA',
+		'az'    => 'PANOPTICON_TINYMCE_LANG_AZ',
+		'be'    => 'PANOPTICON_TINYMCE_LANG_BE',
+		'bg_BG' => 'PANOPTICON_TINYMCE_LANG_BG_BG',
+		'bn_BD' => 'PANOPTICON_TINYMCE_LANG_BN_BD',
+		'ca'    => 'PANOPTICON_TINYMCE_LANG_CA',
+		'cs'    => 'PANOPTICON_TINYMCE_LANG_CS',
+		'cy'    => 'PANOPTICON_TINYMCE_LANG_CY',
+		'da'    => 'PANOPTICON_TINYMCE_LANG_DA',
+		'de'    => 'PANOPTICON_TINYMCE_LANG_DE',
+		'el'    => 'PANOPTICON_TINYMCE_LANG_EL',
+		'eo'    => 'PANOPTICON_TINYMCE_LANG_EO',
+		'es'    => 'PANOPTICON_TINYMCE_LANG_ES',
+		'es_MX' => 'PANOPTICON_TINYMCE_LANG_ES_MX',
+		'et'    => 'PANOPTICON_TINYMCE_LANG_ET',
+		'eu'    => 'PANOPTICON_TINYMCE_LANG_EU',
+		'fa'    => 'PANOPTICON_TINYMCE_LANG_FA',
+		'fi'    => 'PANOPTICON_TINYMCE_LANG_FI',
+		'fr_FR' => 'PANOPTICON_TINYMCE_LANG_FR_FR',
+		'ga'    => 'PANOPTICON_TINYMCE_LANG_GA',
+		'gl'    => 'PANOPTICON_TINYMCE_LANG_GL',
+		'he_IL' => 'PANOPTICON_TINYMCE_LANG_HE_IL',
+		'hr'    => 'PANOPTICON_TINYMCE_LANG_HR',
+		'hu_HU' => 'PANOPTICON_TINYMCE_LANG_HU_HU',
+		'hy'    => 'PANOPTICON_TINYMCE_LANG_HY',
+		'id'    => 'PANOPTICON_TINYMCE_LANG_ID',
+		'is_IS' => 'PANOPTICON_TINYMCE_LANG_IS_IS',
+		'it'    => 'PANOPTICON_TINYMCE_LANG_IT',
+		'ja'    => 'PANOPTICON_TINYMCE_LANG_JA',
+		'ka_GE' => 'PANOPTICON_TINYMCE_LANG_KA_GE',
+		'kab'   => 'PANOPTICON_TINYMCE_LANG_KAB',
+		'kk'    => 'PANOPTICON_TINYMCE_LANG_KK',
+		'ko_KR' => 'PANOPTICON_TINYMCE_LANG_KO_KR',
+		'ku'    => 'PANOPTICON_TINYMCE_LANG_KU',
+		'lt'    => 'PANOPTICON_TINYMCE_LANG_LT',
+		'lv'    => 'PANOPTICON_TINYMCE_LANG_LV',
+		'nb_NO' => 'PANOPTICON_TINYMCE_LANG_NB_NO',
+		'ne'    => 'PANOPTICON_TINYMCE_LANG_NE',
+		'nl'    => 'PANOPTICON_TINYMCE_LANG_NL',
+		'nl_BE' => 'PANOPTICON_TINYMCE_LANG_NL_BE',
+		'oc'    => 'PANOPTICON_TINYMCE_LANG_OC',
+		'pl'    => 'PANOPTICON_TINYMCE_LANG_PL',
+		'pt_BR' => 'PANOPTICON_TINYMCE_LANG_PT_BR',
+		'pt_PT' => 'PANOPTICON_TINYMCE_LANG_PT_PT',
+		'ro'    => 'PANOPTICON_TINYMCE_LANG_RO',
+		'ru'    => 'PANOPTICON_TINYMCE_LANG_RU',
+		'sk'    => 'PANOPTICON_TINYMCE_LANG_SK',
+		'sl_SI' => 'PANOPTICON_TINYMCE_LANG_SL_SI',
+		'sq'    => 'PANOPTICON_TINYMCE_LANG_SQ',
+		'sr'    => 'PANOPTICON_TINYMCE_LANG_SR',
+		'sv_SE' => 'PANOPTICON_TINYMCE_LANG_SV_SE',
+		'ta'    => 'PANOPTICON_TINYMCE_LANG_TA',
+		'tg'    => 'PANOPTICON_TINYMCE_LANG_TG',
+		'th_TH' => 'PANOPTICON_TINYMCE_LANG_TH_TH',
+		'tr'    => 'PANOPTICON_TINYMCE_LANG_TR',
+		'ug'    => 'PANOPTICON_TINYMCE_LANG_UG',
+		'uk'    => 'PANOPTICON_TINYMCE_LANG_UK',
+		'uz'    => 'PANOPTICON_TINYMCE_LANG_UZ',
+		'vi'    => 'PANOPTICON_TINYMCE_LANG_VI',
+		'zh_CN' => 'PANOPTICON_TINYMCE_LANG_ZH_CN',
+		'zh_HK' => 'PANOPTICON_TINYMCE_LANG_ZH_HK',
+		'zh_MO' => 'PANOPTICON_TINYMCE_LANG_ZH_MO',
+		'zh_SG' => 'PANOPTICON_TINYMCE_LANG_ZH_SG',
+		'zh_TW' => 'PANOPTICON_TINYMCE_LANG_ZH_TW',
+	];
 
 	public static function setContainer(Container $container): void
 	{
@@ -86,6 +160,7 @@ abstract class TinyMCE
 				'document_base_url'           => Uri::base(),
 				'link_default_protocol'       => 'https',
 			],
+			self::getTinyMCETranslationOptions(),
 			$options
 		);
 
@@ -94,8 +169,8 @@ abstract class TinyMCE
 
 		$document->addScriptOptions(
 			'panopticon.tinymce.config', [
-			$id => $options,
-		]
+				$id => $options,
+			]
 		);
 
 		$js = <<< JS
@@ -147,5 +222,102 @@ HTML;
 
 		// Include the JavaScript
 		Template::addJs('media://tinymce/tinymce.js', self::$container->application, defer: true);
+	}
+
+	/**
+	 * Attempt to find the most relevant TinyMCE language file.
+	 *
+	 * The preference cascade goes like this (most to least relevant):
+	 * - Exact user's preference, e.g. de-DE
+	 * - Best match user's preference, e.g. de
+	 * - Exact system preference, e.g. de-DE
+	 * - Best match system preference, e.g. de
+	 * - English (default fallback if the language file does not exist)
+	 *
+	 * @return  array
+	 */
+	protected static function getTinyMCETranslationOptions(): array
+	{
+		$langFolder = APATH_MEDIA . '/tinymce/langs';
+
+		if (!file_exists($langFolder) || !is_dir($langFolder) || !is_readable($langFolder))
+		{
+			return [];
+		}
+
+		$availableLanguages = [];
+		$di                 = new \DirectoryIterator($langFolder);
+		/** @var \DirectoryIterator $file */
+		foreach ($di as $file)
+		{
+			if ($file->isDot() || $file->getExtension() !== 'js')
+			{
+				continue;
+			}
+
+			$baseLanguage = $file->getBasename('.' . $file->getExtension());
+
+			if (!isset(self::TINYMCE_LANG_MAP[$baseLanguage]))
+			{
+				continue;
+			}
+
+			$availableLanguages[] = $baseLanguage;
+		}
+
+		$languagePreferences = [
+			self::$container->userManager->getUser()->getParameters()->get('language'),
+			self::$container->language->getLangCode(),
+			'en-GB',
+			'en',
+		];
+
+		$languagePreferences = array_filter($languagePreferences);
+		$languagePreferences = array_unique($languagePreferences);
+
+		$selectedLanguage = null;
+
+		foreach ($languagePreferences as $lang)
+		{
+			$lang = str_replace('-', '_', $lang);
+
+			if (in_array($lang, $availableLanguages))
+			{
+				$selectedLanguage = $lang;
+				break;
+			}
+
+			if (!str_contains($lang, '_'))
+			{
+				continue;
+			}
+
+			[$partialLang, $country] = explode('_', $lang, 2);
+
+			if (in_array($partialLang, $availableLanguages))
+			{
+				$selectedLanguage = $partialLang;
+				break;
+			}
+
+			foreach ($availableLanguages as $mceLang)
+			{
+				if ($mceLang === $partialLang)
+				{
+					$selectedLanguage = $partialLang;
+					break;
+				}
+			}
+		}
+
+		if (empty($selectedLanguage))
+		{
+			return [];
+		}
+
+		return [
+			'language_load' => true,
+			'language'      => $selectedLanguage,
+		];
 	}
 }
