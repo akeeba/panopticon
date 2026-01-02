6051ffd1d5b52c5f33e2e434a616e25f1287e31b
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2026-01-02 12:31:31 +0200

    Happy New Year!
    Signed-off-by: Nicholas K. Dionysopoulos <nicholas@akeeba.com>

--- ViewTemplates/Reports/mail.blade.php
+++ ViewTemplates/Reports/mail.blade.php
@@ -1,3 +1,13 @@
+<?php
+/**
+ * @package   panopticon
+ * @copyright Copyright (c)2023-2026 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   https://www.gnu.org/licenses/agpl-3.0.txt GNU Affero General Public License, version 3 or later
+ */
+
+defined('AKEEBA') || die;
+
+?>
 @include('Mailtemplates/mail_action_summary', [
     'site'    => $this->getContainer()->mvcFactory->makeTempModel('Sites')->findOrFail($this->getModel()->getState('site_id', null)),
     'records' => $this->getModel()->get(true),
--- assets/LocalDev/LocalLibraryLoader.php
+++ assets/LocalDev/LocalLibraryLoader.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @package   panopticon
+ * @copyright Copyright (c)2023-2026 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   https://www.gnu.org/licenses/agpl-3.0.txt GNU Affero General Public License, version 3 or later
+ */
 
 namespace Akeeba\Panopticon\LocalDev;
 
