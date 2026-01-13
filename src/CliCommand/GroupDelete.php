da51ff11f2af20bb354b11a378dd5333ec52524a
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2026-01-13 14:39:35 +0200

    CLI command `group:delete`
    Signed-off-by: Nicholas K. Dionysopoulos <nicholas@akeeba.com>

---
+++ src/CliCommand/GroupDelete.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @package   panopticon
+ * @copyright Copyright (c)2023-2026 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   https://www.gnu.org/licenses/agpl-3.0.txt GNU Affero General Public License, version 3 or later
+ */
+
+namespace Akeeba\Panopticon\CliCommand;
+
+defined('AKEEBA') || die;
+
+use Akeeba\Panopticon\CliCommand\Attribute\ConfigAssertion;
+use Akeeba\Panopticon\Factory;
+use Akeeba\Panopticon\Model\Groups;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+	name: 'group:delete',
+	description: 'Deletes a user group',
+	hidden: false,
+)]
+#[ConfigAssertion(true)]
+class GroupDelete extends AbstractCommand
+{
+	protected function execute(InputInterface $input, OutputInterface $output): int
+	{
+		$container = Factory::getContainer();
+		/** @var Groups $model */
+		$model = $container->mvcFactory->makeTempModel('Groups');
+
+		$id = intval($input->getArgument('id'));
+
+		try
+		{
+			$model
+				->findOrFail($id)
+				->delete();
+		}
+		catch (\Exception $e)
+		{
+			$this->ioStyle->error([
+				sprintf('Could not delete group %d', $id),
+				$e->getMessage()
+			]);
+
+			return Command::FAILURE;
+		}
+
+		$this->ioStyle->success(sprintf('Successfully deleted group %d.', $id));
+
+		return Command::SUCCESS;
+	}
+
+	protected function configure(): void
+	{
+		$this
+			->addArgument('id', InputArgument::REQUIRED, 'The numeric group ID to delete');
+	}
+
+}
